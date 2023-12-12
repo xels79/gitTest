623e8569a0bbbc5f812409eff187438812e4d948
Alexander <xel_s@mail.ru>
2023-12-12 23:02:58 +0300

    Добавили получение данных

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-const temp="test";
-console.log(temp);
+(async function(){
+    const data = (await fetch('https://swapi.dev/api/people').then(response=>response.json())).results;
+    console.log(data);
+})()
